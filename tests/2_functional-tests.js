6b0c6e7695f9ecf3d455ca49999e431fb4416cc7
Per <vbratou@gmail.com>
2023-08-29 09:58:14 +0200

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,1 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-const assert = chai.assert;
-const server = require('../server');
 
-chai.use(chaiHttp);
-
-suite('Functional Tests', function() {
-
-});
